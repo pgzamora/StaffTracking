f3654d20367efc2e77709bca57f4394aa394593f
pgzamora <phillip.zamora@jacks.sdstate.edu>
2020-02-17 13:38:20 -0600

    Initial Commit

---
+++ Staff Tracking appTests/Staff_Tracking_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  Staff_Tracking_appTests.swift
+//  Staff Tracking appTests
+//
+//  Created by Phillip Zamora on 2/17/20.
+//  Copyright © 2020 Phillip Zamora. All rights reserved.
+//
+
+import XCTest
+@testable import Staff_Tracking_app
+
+class Staff_Tracking_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
